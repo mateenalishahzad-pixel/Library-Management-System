af93f6a040e5256b3ead7051f1c0e073981bdf71
mateenalishahzad <mateenalishahzad@gmail.com>
2025-12-04 20:20:50 +0500

    Add Book data model

---
+++ src/models/book.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class Book:
+    isbn: str
+    title: str
+    author: str
+    publication_year: int
+    available: bool = True
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.publication_year})"
