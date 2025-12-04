cb7765f01d844934cc9ccaeb425f31542222a780
mateenalishahzad <mateenalishahzad@gmail.com>
2025-12-04 20:22:25 +0500

    Add User data model

---
+++ src/models/user.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class User:
+    user_id: str
+    name: str
+    borrowed_books: List[str] = field(default_factory=list)
+
+    def __str__(self):
+        return f"{self.name} (ID: {self.user_id})"
